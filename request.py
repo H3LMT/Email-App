c76045d61ce24551f0a13efbf37b310c53303375
H3LMT <vishalbaskar@msn.com>
2018-01-27 22:26:51 -0500

    email implemented

--- request.py
+++ request.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/main')
-def wwwww():
+def home():
 	return render_template('index.html')
+
+@app.route('/about')
+def hi():
+   return render_template('about.html')
 
 @app.route('/hello',methods =['POST'])
 def hello_name():
-   return render_template('about.html')
+   print(request.form['name'])
+   return render_template('return.html',email=request.form['name'])
 
 if __name__ == '__main__':
    app.run(debug = True)
