95a841add1be6a64fb4ebb68a5e33e4138d92963
H3LMT <vishalbaskar@msn.com>
2018-01-28 01:52:42 -0500

    emails send with backup service

--- request.py
+++ request.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request
 import requests
+import sendgrid
+import numpy
+from sendgrid.helpers.mail import *
+
 app = Flask(__name__)
+
+mailgunSandbox = "YOUR_SANDBOX_HERE"
+mailgunKey = "YOUR_KEY_HERE"
+sendgridKey= "YOUR_KEY_HERE"
 
 @app.route('/main')
 def home():
@@ -11,21 +19,62 @@ def hi():
    return render_template('about.html')
 
 @app.route('/submit',methods =['POST'])
-def mailGun():
-	key = 'key-180607584fe0eed06fdcdc3943e3e6eb'
-	sandbox = 'sandbox6521ec12d7024cdab31d45c8b8f47386.mailgun.org'
+def sendMail():
+	global mailgunKey
+	global mailgunSandbox
+	global sendgridKey
+	if(mailgunSandbox=="YOUR_SANDBOX_HERE" or mailgunKey =="YOUR_KEY_HERE" or sendgridKey=="YOUR_KEY_HERE"):
+		mailgunKey = str(numpy.load("./keys/mgkey.npy"))
+		sendgridKey = str(numpy.load("./keys/sg.npy"))
+		mailgunSandbox = str(numpy.load("./keys/mgsbx.npy"))
+
+	#form data
 	name = request.form['name']
-	recepient = request.form['email']
+	sender = request.form['from']
+	recepient = request.form['to']
+	subject = request.form['subject']
 	message = request.form['msg']
+
+	#mail gun info
+	key = mailgunKey
+	sandbox = mailgunSandbox
+
+	#try mailgun
+	provider = "Mailgun"
 	request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(sandbox)
 	pull = requests.post(request_url, auth=('api', key), data={
 	'from':  name+' <excited@samples.mailgun.org>',
 	'to': recepient,
 	'subject': 'Test',
 	'text': message})
+	condition = True
 	print 'Status: {0}'.format(pull.status_code)
 	print 'Body:   {0}'.format(pull.text)
-	return render_template('return.html',email=name)
+
+	if(pull.status_code!=200): #if mailgun fails try sendgrid
+		provider = "SendGrid"
+		sg = sendgrid.SendGridAPIClient(apikey=sendgridKey)
+		from_email = Email("test@example.com")
+		to_email = Email(recepient)
+		subject = "Sending with SendGrid is Fun"
+		content = Content("text/plain", message)
+		mail = Mail(from_email, subject, to_email, content)
+		response = ""
+		try:
+			response = sg.client.mail.send.post(request_body=mail.get())
+			print("WDJOJWD W")
+			print(response.status_code)
+			print(response.body)
+			print(response.headers)
+		except:
+			print("FWJOJOE L")
+			condition = False		
+
+	#status message
+	status = "Your message to <"+recepient+"> was delivered by "+provider+"."
+	if(condition==False):
+		status = "Your message to <"+recepient+"> failed to deilver"
+	return render_template('return.html',message=status)
 
 if __name__ == '__main__':
    app.run(debug = True)
